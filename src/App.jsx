af51c2b23e44d9de2e299b7c5dc5a1296fc1d71a
r12esh <ritesh098765432@gmail.com>
2022-08-12 23:41:53 +0530

    Add navbar desktop

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { Brand, CTA, Navbar } from "./components";
 const App = () => {
   return (
     <div className="App">
-      Hi this is me
       <div className="gradient__bg">
         <Navbar />
         <Header />
